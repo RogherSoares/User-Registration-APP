e8410f721577d5b3b87ecfaae346521e9c4b2553
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-04-02 21:35:47 -0300

    imports inicias do Dao

--- app/src/main/java/com/roghersoares/userregistrationapp/UserDao.java
+++ app/src/main/java/com/roghersoares/userregistrationapp/UserDao.java
@@ -1,4 +1,15 @@
 package com.roghersoares.userregistrationapp;
 
+
+// O Data Access Object é o compnete que serve para a persitência de dados. Serve para definir as operações que podem ser feitas no banco de dados com relação à entidade User
+
+//Importa as notações do Room necessárias para definir o DAO
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+//Importa a classe List do Java para retornar a lista de usuários
+import java.util.List;
+
 public interface UserDao {
 }
