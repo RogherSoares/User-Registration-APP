2b10b72ac7b7dac2351c345c4277b1d0c33c8044
Rogher Adriano Soares <rogher_adriano@hotmail.com>
2026-04-09 19:53:51 -0300

    começo da logica

--- app/src/main/java/com/roghersoares/userregistrationapp/ReportActivity.java
+++ app/src/main/java/com/roghersoares/userregistrationapp/ReportActivity.java
@@ -1,4 +1,26 @@
 package com.roghersoares.userregistrationapp;
 
-public class ReportActivity {
+//Importações de componentes de UI, intenções e a biblioteca
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReportActivity  extends AppCompatActivity {
+    //Campo de texto onde os dados do banco serão exibidos no usages
+    private TextView textViewReport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //Define o Layout XML dessa tela de relatório
+        setContentView(R.layout.activity_report);
+        //MApeamento do TextView do XML para o Java
+        textViewReport = findViewById(R.id.textViewReport);
+        //encontra o botão e define o clique para voltar
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+        //O botão de retorno utilizando expressão lambda
+        btnVoltar.setOnClickListener(v -> voltarParaCadastro());
+
+    }
 }
